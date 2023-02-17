edd0196f92623abb6768de2ba18b1cdd064b9aaf
Juan Felix <juanfelixacosta@outlook.com>
2023-02-17 14:41:33 -0500

    Se realiza el ejercicio 4 de la Guia Java

---
+++ src/introjava/Ejercicio4.java
@@ -0,0 +1,26 @@
+/*
+EJERCICIO 4
+Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables por
+pantalla.
+ */
+package introjava;
+
+/**
+ *
+ * @author juanf
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Juan";
+        byte edad = 41;
+        
+        System.out.println("Tu nombre es " + nombre);
+        System.out.println("Tienes " + edad + " años");
+    }
+    
+}

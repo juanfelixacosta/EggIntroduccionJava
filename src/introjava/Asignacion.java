ec2ad91f52e9e39d47771f3de9734f2198b8b9c0
Juan Felix <juanfelixacosta@outlook.com>
2023-02-17 14:22:23 -0500

    Se realiza el ejercicio 2 de la Guia Java

---
+++ src/introjava/Asignacion.java
@@ -0,0 +1,27 @@
+/*
+EJERCICIO 2
+¿Recuerdas las variables que creaste en el ejercicio anterior? Ahora deberás asignarles un valor.
+ */
+package introjava;
+
+/**
+ *
+ * @author juanf
+ */
+public class Asignacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         //Creando variables y asignando valores
+        String nomnbre = "Irene";
+        int numero = 4;
+        double precio = 15.25;
+        byte edad = 41;
+        char caracter = 'M';
+        boolean casado = true;
+    }
+    
+}

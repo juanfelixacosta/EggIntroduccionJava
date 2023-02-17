6fcd43b40b9e577568147915fcc7b15855564f7c
Juan Felix <juanfelixacosta@outlook.com>
2023-02-17 14:12:42 -0500

    Creacion del repositorio

---
+++ src/introjava/Variables.java
@@ -0,0 +1,27 @@
+/*
+    EJERCICIO 1
+    Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+ */
+package introjava;
+
+/**
+ *
+ * @author juanf
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Creando variables
+        String nomnbre;
+        int numero;
+        double precio;
+        byte edad;
+        char caracter;
+        boolean casado;
+    }
+    
+}

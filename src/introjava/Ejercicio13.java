b721a4f4846f5f672f5da2c498f115c759f9a9f1
Juan Felix <juanfelixacosta@outlook.com>
2023-02-21 20:37:22 -0500

    Se crea Ejercicio 13 Guia Java

---
+++ src/introjava/Ejercicio13.java
@@ -0,0 +1,22 @@
+/*
+EJERCICIO 13
+Crea un vector llamado ‘Equipo’ cuya dimensión sea la cantidad de compañeros de equipo y
+define su tipo de dato de tal manera que te permita alojar sus nombres más adelante.
+ */
+package introjava;
+
+/**
+ *
+ * @author juanf
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] equipo = new String[9]; 
+    }
+    
+}
